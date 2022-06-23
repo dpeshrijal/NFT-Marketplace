dfffd8e7c56205aa4f51467902e09ed255e15132
Deepesh Rijal <deepeshrijal@gmail.com>
2022-06-23 21:03:09 +0545

    wrote function to create nft listing

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -10,8 +10,38 @@ contract NFTMarketplace is ReentrancyGuard {
     uint256 public immutable feePercent;
     uint256 public itemCount;
 
+    struct Item {
+        uint256 itemId;
+        IERC721 nft;
+        uint256 tokenId;
+        uint256 price;
+        address payable seller;
+        bool sold;
+    }
+
+    mapping(uint256 => Item) public items;
+
     constructor(uint256 _feePercent) {
         feeAccount = payable(msg.sender);
         feePercent = _feePercent;
+    }
+
+    function marketItem(
+        IERC721 _nft,
+        uint256 _tokenId,
+        uint256 _price
+    ) external nonReentrant {
+        require(_price > 0, "Cannot sale your NFT for free!");
+        itemCount++;
+        _nft.transferFrom(msg.sender, address(this), _tokenId);
+
+        items[itemCount] = Item(
+            itemCount,
+            _nft,
+            _tokenId,
+            _price,
+            payable(msg.sender),
+            false
+        );
     }
 }
