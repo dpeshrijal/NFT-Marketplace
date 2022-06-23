2c8e6061fd3fc253cfb4af7e8f1e10276e56572c
Deepesh Rijal <deepeshrijal@gmail.com>
2022-06-23 12:50:32 +0545

    Debugged some error

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -5,12 +5,13 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 pragma solidity ^0.8.4;
 
-contract NFTMarketplace is ReentrancyGuard {}
+contract NFTMarketplace is ReentrancyGuard {
     address payable public immutable feeAccount;
     uint256 public immutable feePercent;
     uint256 public itemCount;
 
-    constructor(uint256 _feePercent){
-        feeAccount=payable(msg.sender);
+    constructor(uint256 _feePercent) {
+        feeAccount = payable(msg.sender);
         feePercent = _feePercent;
     }
+}
