6e02bacc2c525fb4002b54864d085cd2a095e81f
Deepesh Rijal <deepeshrijal@gmail.com>
2022-06-23 12:51:03 +0545

    added deploy command for nftMarketplace contract.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,10 +2,16 @@
 
 const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory("NFT");
+    const nftMarketplaceContractFactory = await hre.ethers.getContractFactory("NFTMarketplace");
+
     const nftContract = await nftContractFactory.deploy();
+    const nftMarketplaceContract = await nftMarketplaceContractFactory.deploy();
+
     await nftContract.deployed();
+    await nftMarketplaceContract.deployed(1);
 
     console.log("NFT contract is deployed to:", nftContract.address);
+    console.log("NFTMarketplace contract is deployed to:", nftMarketplaceContract.address);
 }
 
 const runMain = async () => {
