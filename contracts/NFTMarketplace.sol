57cc75c8156b70b79374169b5e3380a6de66fef3
Deepesh Rijal <deepeshrijal@gmail.com>
2022-06-23 12:04:28 +0545

    Started Writing the marketplace contract

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -1,1 +1,8 @@
 //SPDX-License-Identifier:MIT
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+pragma solidity ^0.8.4;
+
+contract NFTMarketplace is ReentrancyGuard {}
